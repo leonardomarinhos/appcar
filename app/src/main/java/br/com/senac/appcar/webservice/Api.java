62e4a0008281b38fb83be2f5c27dc4049f2e7d2a
LEONARDO MARINHO DE SOUSA <leonardo.msousa2@senacsp.edu.br>
2019-06-27 21:41:59 -0300

    Initial commit

---
+++ app/src/main/java/br/com/senac/appcar/webservice/Api.java
@@ -0,0 +1,11 @@
+package br.com.senac.appcar.webservice;
+
+public class Api {
+    private static final String ROOT_URL = "https://thainagostosa.000webhostapp.com/appcar/v1/Api.php?apicall=";
+
+    public static final String URL_CREATE_APPCAR = ROOT_URL + "createappcar";
+    public static final String URL_READ_APPCAR = ROOT_URL + "getappcar";
+    public static final String URL_UPDATE_APPCAR = ROOT_URL + "updateappcar";
+    public static final String URL_DELETE_APPCAR = ROOT_URL + "deleteappcar&id=";
+
+}
